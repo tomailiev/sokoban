01d146882bc5713ea461e54d9950b103b1a6d19b
tomailiev <iliev.toma@gmail.com>
2021-04-03 10:00:55 -0700

    added new assets

--- src/themes/pixelated.js
+++ src/themes/pixelated.js
@@ -2,7 +2,7 @@ import box from "../assets/pixelated/box.png";
 import player from '../assets/pixelated/player.png';
 import path from '../assets/pixelated/path.jpg';
 import goal from '../assets/pixelated/goal.png';
-import brick from '../assets/pixelated/brick.webp';
+import brick from '../assets/pixelated/brick.png';
 
 const pixelatedPics = {
     box,
